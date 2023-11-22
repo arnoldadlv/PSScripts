be043994c882e617b4d13d1d0b32e259e757b4df
arnoldadlv <147350124+arnoldadlv@users.noreply.github.com>
2023-11-22 10:49:12 -0800

    Add files via upload

---
+++ getUsersWithZDrive.ps1
@@ -0,0 +1,9 @@
+﻿# need Active Directory Module
+
+$users = Get-ADUser -Filter * -Properties HomeDirectory
+
+foreach ($user in $users) {
+    if ($user.HomeDirectory -like '*Z*') {
+    Write-Host "User $($user.samaccountname) has a Z: Drive"
+   }
+}
